dc426958716b23ab3484c5cc298191b0ff828c8c
zsoerenm <zsoerenm@hotmail.de>
2018-10-31 16:54:18 +0100

    Copy Magento into the container does not error anymore

--- compose/magento-2-windows/bin/copymagento.ps1
+++ compose/magento-2-windows/bin/copymagento.ps1
@@ -1,1 +1,1 @@
-docker-compose run --user=root --no-deps --rm -v $PWD/src/.:/source phpfpm cp -r /src/. /var/www/html
+docker-compose run --user=root --no-deps --rm -v $PWD/src/.:/src phpfpm cp -r /src/. /var/www/html
