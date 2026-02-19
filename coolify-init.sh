3e398e77ad4d0408ce1ae2c1fe197d75b3e5f738
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2026-02-19 17:48:27 +0530

    Create coolify-init.sh

---
+++ coolify-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Finding PHPFPM container..."
+
+PHP_CONTAINER=$(docker ps \
+  --filter "ancestor=markoshust/magento-php:8.3-fpm-4" \
+  --format "{{.Names}}" | head -n1)
+
+echo "Container found: $PHP_CONTAINER"
+
+docker exec "$PHP_CONTAINER" bash -lc "
+cd /var/www/html &&
+bash bin/init
+"
